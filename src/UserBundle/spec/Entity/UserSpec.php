1a926e0de3c99494724f76f2951f17c8fc2039c1
Arminek <arkadiusz.k.e@gmail.com>
2016-08-13 14:45:11 +0200

    [User] Add user spec

---
+++ src/UserBundle/spec/Entity/UserSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace spec\UserBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use UserBundle\Entity\User;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * @mixin User
+ */
+class UserSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(User::class);
+    }
+
+    function it_implements_user_interface()
+    {
+        $this->shouldImplement(UserInterface::class);
+    }
+
+    function it_has_username()
+    {
+        $this->setUsername('John doe');
+
+        $this->getUsername()->shouldReturn('John doe');
+    }
+
+    function it_has_password_and_plain_password()
+    {
+        $this->setPassword('john123');
+        $this->setPlainPassword('john123');
+
+        $this->getPassword()->shouldReturn('john123');
+        $this->getPlainPassword()->shouldReturn('john123');
+    }
+
+    function it_has_email()
+    {
+        $this->setEmail('john@example.com');
+
+        $this->getEmail()->shouldReturn('john@example.com');
+    }
+
+    function it_has_activation_code()
+    {
+        $this->setActivationCode('xyz');
+
+        $this->getActivationCode()->shouldReturn('xyz');
+    }
+}
